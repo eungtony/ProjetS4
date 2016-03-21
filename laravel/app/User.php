2785916a0e1d95b0b36d8983297988341f56d773
Tony <eungtony@outlook.fr>
2016-03-21 10:29:57 +0100

    modification du setAvatar et du getAvatar

--- laravel/app/User.php
+++ laravel/app/User.php
@@ -29,16 +29,16 @@ class User extends Authenticatable
 
     public function getAvatarAttribute($avatar){
         if($avatar){
-            return "img/avatars/{$this->id}.jpg";
+            return "{$this->id}.jpg";
         }else{
-            false;
+            return "default.png";
         }
     }
 
     public function setAvatarAttribute($avatar){
         if(is_object($avatar) && $avatar->isValid()){
             //dd(File::exists(storage_path(public_path()."\img\avatars\".{$this->id}.jpg)));
-            ImageManagerStatic::make($avatar)->fit(150,150)->save(public_path()."/img/avatars/{$this->id}.jpg");
+            ImageManagerStatic::make($avatar)->fit(150,150)->save("/home/tony/public_html/img/avatars/{$this->id}.jpg");
             $this->attributes['avatar'] = true;
         }
     }
@@ -70,7 +70,7 @@ class User extends Authenticatable
     }
 
     public function isAdmin(){
-        return $this->role_id == '3';
+        return $this->statut_id == '2';
     }
 
 }
